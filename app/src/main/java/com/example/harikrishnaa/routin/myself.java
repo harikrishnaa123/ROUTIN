3b0549a458f7e5705f6617d95ce72dc0b4ace774
harikrishnaa <kvharikrishnaa@gmail.com>
2018-08-30 15:54:56 +0530

    test2

--- app/src/main/java/com/example/harikrishnaa/routin/myself.java
+++ app/src/main/java/com/example/harikrishnaa/routin/myself.java
@@ -1,23 +1,25 @@
 package com.example.harikrishnaa.routin;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.TextView;
 
 
 public class myself extends Fragment {
+    TextView profile;
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_myself, container, false);
+        View view = inflater.inflate(R.layout.fragment_myself, container, false);
+        return view;
     }
 
 
--- app/src/main/java/com/example/harikrishnaa/routin/signup.java
+++ app/src/main/java/com/example/harikrishnaa/routin/signup.java
@@ -1,6 +1,7 @@
 package com.example.harikrishnaa.routin;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,23 +15,30 @@ public class signup extends AppCompatActivity {
 
     ImageView person_image;
     Button signup_button;
-    EditText entered_email;
+    EditText entered_email, entered_name, referral_code;
+
+    /*public static final String DETAILS_PREFS = "details";
+    public static final String NAME_KEY = "username";
+    public static final String EMAIL_KEY = "useremail";
+    public static final String REFERRAL_KEY = "userreferral";*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-
         final String email;
 
         person_image = findViewById(R.id.person);
         signup_button = findViewById(R.id.signup);
         entered_email = findViewById(R.id.email);
+        entered_name = findViewById(R.id.username);
+        referral_code = findViewById(R.id.referralcode);
         email = entered_email.getText().toString();
 
         signup_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //saveUserDetails();
                 if(email.contains("@") && (!email.isEmpty())){
                     Toast.makeText(getApplicationContext(), "Enter a valid email address", Toast.LENGTH_SHORT).show();
             }else{
@@ -46,17 +54,33 @@ public class signup extends AppCompatActivity {
 
     public void onRadioButtonClicked(View view) {
         boolean checked = ((RadioButton) view).isChecked();
-
         // Check which radio button was clicked
         switch(view.getId()) {
             case R.id.male:
-                if (checked)
+                if (checked) {
                     person_image.setImageResource(R.drawable.male);
+                    //p.getIcon("male");
                     break;
+                }
             case R.id.female:
-                if (checked)
+                if (checked) {
                     person_image.setImageResource(R.drawable.female);
+                    //p.getIcon("female");
                     break;
+                }
         }
     }
+    /*private void saveUserDetails(){
+        String name = entered_name.getText().toString();
+        String email = entered_email.getText().toString();
+        String code = referral_code.getText().toString();
+        profile p = new profile();
+        SharedPreferences prefs = getSharedPreferences(DETAILS_PREFS, 0);
+        prefs.edit().putString(NAME_KEY, name).apply();
+        p.getName();
+        prefs.edit().putString(EMAIL_KEY, email).apply();
+        p.getEmail();
+        prefs.edit().putString(REFERRAL_KEY, code);
+        p.getReferalCode();
+    }*/
 }
